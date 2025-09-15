095895d83bcd93f961cb4f1d9a09042ab9f7fb5e
Drew Krehel <drewkrehel@gmail.com>
2025-09-15 20:23:55 +0000

    working!

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -12,6 +12,14 @@ class ItemsController < ApplicationController
 
   def insert
 
+    @new_link = Item.new
+
+    @new_link.link_url = params[:query_link_url] 
+    @new_link.link_description = params[:query_link_description]
+    @new_link.thumbnail_url = params[:query_thumbnail_url] 
+
+    @new_link.save
+
     redirect_to("/")
   end
 end
